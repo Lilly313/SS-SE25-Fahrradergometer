9519f7acaa28a563f968d5df0c54f61768306b55
id-dp <i.duerr-pucher@mci4me.at>
2025-03-24 12:47:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from my_functions import estimate_max_hr
+if __name__== "__main__":
+    print(estimate_max_hr(20,"female"))
+
 from my_functions import build_person
 if __name__== "__main__":
-    print(build_person("Ida","Dürr-Pucher","female",20)) 
+    print(build_person("Ida","Dürr-Pucher","female",20))
+
+from my_functions import build_experiment
+if __name__== "__main__":
+    print(build_experiment("experiment","24.03.2025","Julian Huber","Software Engineering"))
