6988d4092beab2671916a853d007608ff48fcd92
id-dp <i.duerr-pucher@mci4me.at>
2025-04-08 12:33:26 +0200

    Aufgabe61.1

--- main_Ida.py
+++ main_Ida.py
@@ -1,12 +1,12 @@
-from my_functions import estimate_max_hr
+from my_functions_Ida import estimate_max_hr
 if __name__== "__main__":
     print(estimate_max_hr(20,"female"))
 
-from my_functions import build_person
+from my_functions_Ida import build_person
 if __name__== "__main__":
     print(build_person("Ida","Dürr-Pucher","female",20))
 
-from my_functions import build_experiment
+from my_functions_Ida import build_experiment
 if __name__== "__main__":
     print(build_experiment("experiment","24.03.2025","Julian Huber","Software Engineering"))
 
