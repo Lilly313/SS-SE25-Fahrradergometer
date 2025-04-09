08d9834590ad77c5002f27f8a07d819f96ab8f10
Lilly313 <fl1620@mci4me.at>
2025-04-09 13:02:24 +0200

    Aufgabe62

--- my_classes.py
+++ my_classes.py
@@ -1,25 +1,33 @@
 from my_functions_Lilly import estimate_max_hr
+from datetime import datetime
 
-class Subject():
-    def __init__(self, first_name, last_name, sex, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.sex = sex
-        self.age = age
-
-    def estimate_max_hr(self):
-        return estimate_max_hr(self.age, self.sex)
-    
-class Supervisor():
+class Person():
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
+class Subject(Person):
+    def __init__(self, first_name, last_name, dateofbirth, sex):
+        super().__init__(first_name, last_name)   
+        self.__dateofbirth = dateofbirth
+        self.sex = sex
+    
+    def get_age(self):  #orientiert an skript
+        age =  datetime.today().year- self.__dateofbirth.year
+        return age
+    
+    def estimate_max_hr(self):
+        return estimate_max_hr(self.get_age(), self.sex)
+    
+class Supervisor(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
 
 class Experiment():
-    def __init__(self, experiment_name, date):
+    def __init__(self, experiment_name, experiment_date):
         self.experiment_name = experiment_name
-        self.date = date
+        self.experiment_date = experiment_date
 
     def add_subject(self, subject):
         self.subject = subject
@@ -28,4 +36,4 @@ class Experiment():
         self.supervisor = supervisor
 
     def __str__(self):
-        return f"Experiment Name: {self.experiment_name}\nDate: {self.date}\nSubject: {self.subject.first_name} {self.subject.last_name}\nSupervisor: {self.supervisor.first_name} {self.supervisor.last_name}"
+        return f"Experiment Name: {self.experiment_name}\nDate: {self.experiment_date}\nSubject: {self.subject.first_name} {self.subject.last_name}\nSupervisor: {self.supervisor.first_name} {self.supervisor.last_name}"
--- test_Lilly.py
+++ test_Lilly.py
@@ -1,12 +1,18 @@
 from my_classes import Subject, Supervisor, Experiment
+from datetime import datetime
+
 if __name__ == "__main__":
 
     # Erstellen eines Leistungstests
     supervisor = Supervisor("Julian", "Huber")
-    subject = Subject("Lilly", "Feifel", "female", 20)
-    subject.estimate_max_hr()
 
-    experiment = Experiment("assignment61_max_heartrate", "2025-04-04")
+    dateofbirth = datetime.strptime("31.03.2005", "%d.%m.%Y")  #orientiert an skript
+    subject = Subject("Lilly", "Feifel", dateofbirth, "female")
+
+    print("Geschätzte maximale Herzfrequenz:", subject.estimate_max_hr(), "bpm")
+
+    experiment_date = datetime.strptime("09.04.2025", "%d.%m.%Y")
+    experiment = Experiment("assignment62_max_heartrate", "2025-04-09")
     experiment.add_subject(subject)
     experiment.add_supervisor(supervisor)
 
